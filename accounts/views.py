a2ae3f3dfae65dbeac922ae08489f75099935303
rakesh <rakeshsinghkushwah09@gmail.com>
2020-04-27 13:21:42 +0530

    a

--- accounts/views.py
+++ accounts/views.py
@@ -409,5 +409,5 @@ def cancel_add_card(req,id):
 def simple_node(req):
     data = req.GET['name']
     datas = simple_node_data.objects.create(name = data)
-    print(data)
-    return render(req,'accounts/simple_node.html')
+    show_data = simple_node_data.objects.all()
+    return render(req,'accounts/simple_node.html',{'data':show_data})
